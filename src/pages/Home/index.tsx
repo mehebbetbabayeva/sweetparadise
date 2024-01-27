3065ce4069c84aa18ec7dd85c6d70d1123fd0244
mehebbetbabayeva <mhbbtbabaeva97@gmail.com>
2024-01-27 15:21:54 +0400

    welcome page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,16 +3,15 @@ import PageContainer from "../../Pagecontainer"
 import Services from "../../components/Services"
 
 import ServicesSteps from "../../components/ServicesSteps"
+import WelcomeBanner from "../../components/WelcomeBanner"
 
 
 
 const Home = () => {
     return (
         <PageContainer>
-
-            <Services/>
-
-        
+            <WelcomeBanner />
+            <Services />
             <ServicesSteps />
 
         </PageContainer>
