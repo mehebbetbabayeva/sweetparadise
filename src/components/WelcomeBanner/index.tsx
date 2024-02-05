5eae9c9afa306fadd51ed1a9d9c7e8903c61caba
mehebbetbabayeva <mhbbtbabaeva97@gmail.com>
2024-02-05 14:32:34 +0400

    solve bugs

--- src/components/WelcomeBanner/index.tsx
+++ src/components/WelcomeBanner/index.tsx
@@ -6,23 +6,24 @@ import Bannerimg from "../../assets/images/Ellipse 2.png"
 
 const WelcomeBanner = () => {
     return (
-        <div className={styles.banner_page}>
-            <div className="container">
-                <div className={styles.banner_container}>
-                    <div className={styles.banner_text}>
-                        <h2 className={styles.logo}>Sweet Paradise</h2>
-                        <h1 className={styles.welcome_text}>Biz həyatınızı daha da şirin edəcəyik!</h1>
-                        <div className={styles.welcome_button}><Link to="">  <PurpleButton title="Desertləri gör" /></Link></div>
-                    </div>
-                    <div className={styles.banner_carousel}>
-                        <BannerCarousel />
-                    </div>
-                    <div className={styles.banner_img}>
-                        <img src={Bannerimg} alt="banner" />
-                    </div>
+
+        <div className="container">
+            <div className={styles.banner_container}>
+                <div className={styles.banner_text}>
+                    <h2 className={styles.logo}>Sweet Paradise</h2>
+                    <h1 className={styles.welcome_text}>Biz həyatınızı daha da şirin edəcəyik!</h1>
+                    <div className={styles.welcome_button}><Link to="">  <PurpleButton title="Desertləri gör" /></Link></div>
+                </div>
+
+                <div className={styles.banner_carousel}>
+                    <BannerCarousel />
+                </div>
+                <div className={styles.banner_img}>
+                    <img src={Bannerimg} alt="banner" />
                 </div>
             </div>
         </div>
+
     )
 }
 
